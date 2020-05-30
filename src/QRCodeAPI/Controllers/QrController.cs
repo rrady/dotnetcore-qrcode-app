762a5d95bbb699cf5bb17a2097d9274f91afb299
Radu Razvan <rradu08@outlook.com>
2020-05-30 14:21:22 +0300

    swagger + init tests proj

--- src/QRCodeAPI/Controllers/QrController.cs
+++ src/QRCodeAPI/Controllers/QrController.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using QRCodeAPI.Services;
 
 namespace QRCodeAPI.Controllers
 {
-    [Route("api/qr")]
+    [Route("api")]
     public class QrController : ControllerBase
     {
         private readonly IQrService _service;
@@ -14,13 +15,33 @@ namespace QRCodeAPI.Controllers
             _service = service;
         }
         
+        /// <summary>
+        ///    Gets the encoded message from a QR code by the path for a QR image.
+        /// </summary>
+        /// <remarks>
+        ///    Sample request:
+        ///         GET /api/qr/scan?path=code.png
+        ///
+        ///     Sample response:
+        ///         {
+        ///             "data": "HelloWorld",
+        ///             "error": null
+        ///         }
+        /// </remarks>
+        /// <param name="path">The path for QR image.</param>
+        /// <returns></returns>
+        /// <response code="200">Returns the encoded message.</response>
+        /// <response code="400">If the QR image was not found or the QR couldn't be decoded.</response>
         [HttpGet("scan")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")]
         public async Task<IActionResult> ScanQr([FromQuery(Name = "path")] string path)
         {
             var result = await _service.ScanQrAsync(path);
             if (!string.IsNullOrWhiteSpace(result.Error))
             {
-                return NotFound(result);
+                return BadRequest(result);
             }
 
             return Ok(result);
--- src/QRCodeAPI/Startup.cs
+++ src/QRCodeAPI/Startup.cs
@@ -1,10 +1,13 @@
+using System;
 using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using QRCodeAPI.CacheStore;
 using QRCodeAPI.Client;
 using QRCodeAPI.Client.Types;
@@ -31,6 +34,13 @@ namespace QRCodeAPI
             services.AddTransient<IQrService, GoQrService>();
             services.AddMemoryCache();
             services.AddTransient<ICacheStore, InMemoryCacheStore>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "QR Code API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -41,6 +51,11 @@ namespace QRCodeAPI
             }
 
             app.UseMiddleware<ErrorHandlerMiddleware>();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "QR Code API v1");
+            });
             app.UseRouting();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
